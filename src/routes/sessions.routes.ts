da01efdf1cef583616da8ffb153735b8388cc3e4
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2020-12-07 19:45:49 -0300

    Added user avatar on return of sign in route

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -20,6 +20,7 @@ sessionsRouter.post('/', async (request, response) => {
     email: user.email,
     created_at: user.created_at,
     updated_at: user.updated_at,
+    avatar: user.avatar
   };
 
   return response.json({ user: userWithoutPassword, token });
